546f39e63687c460bccc616dd4bdda0b523c9abb
ActionArmadillo <62772766+ActionArmadillo@users.noreply.github.com>
2021-04-25 11:10:49 +0200

    added a vunch of tests from validating and client creation

--- cypress/integration/po-tests/clients.spec.js
+++ cypress/integration/po-tests/clients.spec.js
@@ -2,18 +2,38 @@
 
 import * as loginFunctions from '../../pages/loginPage'
 import * as dashboardFunctions from '../../pages/dashboardPage'
+import * as clientFunctions from '../../pages/clientsPage'
 
-describe('Login and logout tests', () => {
+
+describe('Clients page and New Client tests', () => {
     beforeEach(() => {
         cy.log('before each')
         cy.visit('http://localhost:3000/login')
+        loginFunctions.login('tester01','GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
     })
-
+    /*
     afterEach(() => {
         cy.log('after each')
         dashboardFunctions.performLogout()
     })
+    */
+    it('View Client page', () => {
+        
+        clientFunctions.viewClientsPage('Clients')
+        dashboardFunctions.performLogout()
+    })
 
-    //it()
+    it('View Create Client page', () => {
+        
+        clientFunctions.openClientsPage()
+        clientFunctions.viewNewClientPage()
+        dashboardFunctions.performLogout()
+    })
+
+    it('Create new Client', () => {
+        clientFunctions.openClientsPage()
+        clientFunctions.openNewClientPage()
+        clientFunctions.createNewClient('FIRSTNAME LASTNAME','firstname@lastname.com','123456789')
+    })
 
 })
